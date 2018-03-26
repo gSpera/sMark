1bac30ab83d865fa217218e47d67acd1d00aecbb
Giovanni Spera <giovanni.spera2011@libero.it>
2018-03-26 21:30:07 +0200

    Added utils.go for the command line aerguments

---
+++ utils/utils.go
@@ -0,0 +1,35 @@
+package eNote
+
+import "html/template"
+
+//Options are the command line parameters
+type Options struct {
+	InputFile  *string
+	OutputFile *string
+	NewLine    *bool
+	CustomCSS  *string
+	InlineCSS  *string
+	EnableFont *bool
+}
+
+//OptionsTemplate is a copy of Options but members arte not pointer
+type OptionsTemplate struct {
+	InputFile  string
+	OutputFile string
+	NewLine    bool
+	CustomCSS  template.URL
+	InlineCSS  template.CSS
+	EnableFont bool
+}
+
+//ToTemplate returtns a copy of Options with out pointers
+func (o Options) ToTemplate() OptionsTemplate {
+	return OptionsTemplate{
+		InputFile:  *o.InputFile,
+		OutputFile: *o.OutputFile,
+		NewLine:    *o.NewLine,
+		CustomCSS:  template.URL(*o.CustomCSS),
+		InlineCSS:  template.CSS(*o.InlineCSS),
+		EnableFont: *o.EnableFont,
+	}
+}
