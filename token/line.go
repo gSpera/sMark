64de36edfe02ea440a2459a620c8f3bd486c73d1
Giovanni Spera <giovanni.spera2011@libero.it>
2018-03-30 22:21:02 +0200

    Started organizing token in different files

---
+++ token/line.go
@@ -0,0 +1,37 @@
+package token
+
+import "fmt"
+
+//LineToken is a special interface that indicates a token
+type LineToken interface{ IsToken() }
+
+//HeaderLine rapresent a header line
+type HeaderLine struct {
+	LineToken
+}
+
+//LineContainer is a token which rappresent a list of Tokens with some attributes
+type LineContainer struct {
+	LineToken
+	LineState
+	Tokens []Token
+}
+
+//Type return the type of the Token
+func (t LineContainer) Type() Type { return TypeTokenLine }
+
+func (t LineContainer) String() string {
+	str := ""
+	for i := uint(0); i < t.LineState.Indentation; i++ {
+		str += "\t"
+	}
+	for _, t := range t.Tokens {
+		str += fmt.Sprintf("%v", t)
+	}
+	return str + "\n"
+}
+
+//DebugString is used for ast outputting
+func (t LineContainer) DebugString() string {
+	return fmt.Sprintf("<LineToken: {%+v}%s>\n", t.LineState, t.Tokens)
+}
--- token/token.go
+++ token/token.go
@@ -52,9 +52,6 @@ func (t UndefinedToken) String() string { return "UNDEFINED" }
 //DebugString is used for ast outputting
 func (t UndefinedToken) DebugString() string { return "<UndefinedToken>" }
 
-//LineToken is a special interface that indicates a token
-type LineToken interface{ IsToken() }
-
 //BoldToken is a generic undefined token
 type BoldToken struct{}
 
@@ -126,31 +123,6 @@ func (t TextToken) DebugString() string { return fmt.Sprintf("<TextToken: %s>\n"
 //LineState is the line metadata
 type LineState struct {
 	Indentation uint
-}
-
-//LineContainer is a token which rappresent a list of Tokens with some attributes
-type LineContainer struct {
-	LineState
-	Tokens []Token
-}
-
-//Type return the type of the Token
-func (t LineContainer) Type() Type { return TypeTokenLine }
-
-func (t LineContainer) String() string {
-	str := ""
-	for i := uint(0); i < t.LineState.Indentation; i++ {
-		str += "\t"
-	}
-	for _, t := range t.Tokens {
-		str += fmt.Sprintf("%v", t)
-	}
-	return str + "\n"
-}
-
-//DebugString is used for ast outputting
-func (t LineContainer) DebugString() string {
-	return fmt.Sprintf("<LineToken: {%+v}%s>\n", t.LineState, t.Tokens)
 }
 
 //ParagraphToken rapresent a single paragraph
