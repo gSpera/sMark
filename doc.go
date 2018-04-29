e13fc7c66c39ca83f51eb0e94241a2271339f39a
Giovanni Spera <giovanni.spera2011@libero.it>
2018-04-29 16:21:41 +0200

    Added a little of documentation

---
+++ doc.go
@@ -0,0 +1,4 @@
+//eNote is a markup language which aims to be human readble and more powerfull that competitor
+//this is the official implemntation which currently supports two output engines: HTML and Telegra.ph.
+//eNote is a WIP project
+package main
